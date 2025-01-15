6ed67e3c8964e9c2cbb5e0a33cffd9f742adeec6
Rémi <47191601+remi-garcia@users.noreply.github.com>
2025-01-15 10:36:58 +0100

    Issue with wait=false and stout

--- Julia/run_with_timeout.jl
+++ Julia/run_with_timeout.jl
@@ -6,13 +6,21 @@ function run_with_timeout(command; timeout::Int=0, kwargs...)
         cmd = run(command; wait=true)
         return success(cmd)
     end
-    cmd = run(command; wait=false)
+    pout = Pipe()
+    cmd = run(pipeline(command; stdout = pout); wait=false)
     start_time = time_ns()
     while (time_ns()-start_time)*1e-9 < timeout
         if !process_running(cmd)
+            close(pout.in)
+            println(read(pout, String))
             return success(cmd)
         end
-        sleep(1)
+        sleep(0.1)
+    end
+    if !process_running(cmd)
+        close(pout.in)
+        println(read(pout, String))
+        return success(cmd)
     end
     kill(cmd)
     return false
