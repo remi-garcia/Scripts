d55302576cd5d784effa93cd1d4b08a096e56240
Rémi <47191601+remi-garcia@users.noreply.github.com>
2025-01-15 10:59:52 +0100

    timelimit Type

--- Julia/run_with_timeout.jl
+++ Julia/run_with_timeout.jl
@@ -1,15 +1,15 @@
 # run with timeout
 # https://discourse.julialang.org/t/53876/3
 
-function run_with_timeout(command; timeout::Int=0, kwargs...)
-    if timeout == 0
+function run_with_timeout(command; timelimit::T=0.0, kwargs...) where T <: Real
+    if iszero(timelimit)
         cmd = run(command; wait=true)
         return success(cmd)
     end
     pout = Pipe()
     cmd = run(pipeline(command; stdout = pout); wait=false)
     start_time = time_ns()
-    while (time_ns()-start_time)*1e-9 < timeout
+    while (time_ns()-start_time)*1e-9 < timelimit
         if !process_running(cmd)
             close(pout.in)
             println(read(pout, String))
